78d15ae6d631bc8cfd6ae865ac8b51747509df80
chyngyz <chingiz.usonuulu@gmail.com>
2021-07-01 17:45:24 +0600

    Applications for registering infected and vaccinated citizens

--- final_exam_task2/src/main/java/com/example/demo/request/RequestService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.request;
-
-import java.util.List;
-
-public interface RequestService {
-    Request create(Request request);
-    Integer counter();
-    List<Request> getAllRequests();
-}
